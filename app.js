d2eb8663b3aa82a8a40c90a2ae0a5145dd4d7bfb
mossssama <mo99saleh@gmail.com>
2023-10-03 01:51:59 +0300

    Adding stringQuote endPoint + comments

--- app.js
+++ app.js
@@ -4,16 +4,40 @@ const quotes = require('./quotes');
 const paginateQuotes = require('./functions');
 const PORT = process.env.PORT || 3000;
 
-
+/* Get Random Quote */
 app.get('/randomQuote', (req, res) => {
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
     res.status(200).json(randomQuote);
 });
 
+/* Get Quote using ID */
+app.get('/quote/:id', (req, res) => {
+    const quoteId = parseInt(req.params.id);
+    const quote = quotes.find(q => q.id == quoteId);
+
+    if (!quote)    res.status(404).json({ message: "Quote not found" });
+    else           res.status(200).json(quote);
+    
+});
+
+/* Get Quote using ID as String */
+app.get('/stringQuote/:id', (req, res) => {
+    const quoteId = parseInt(req.params.id);
+    const quote = quotes.find(q => q.id == quoteId);
+
+    if (!quote)     res.status(404).json({ message: "Quote not found" });
+    else {
+        const { text, author } = quote;
+        res.status(200).send(`${text} \n\n\n { ${author} }`);
+    }
+});
+
+/* Get All Quotes */
 app.get('/allQuotes', (req, res) => {
     res.status(200).json(quotes);
 });
 
+/* Get Quotes Page */
 app.get('/quotesPage', (req, res) => {
     const page = parseInt(req.query.page) || 1; 
     const pageSize = parseInt(req.query.pageSize) || 10;
@@ -23,14 +47,6 @@ app.get('/quotesPage', (req, res) => {
     res.status(200).json(paginatedQuotes);
 });
 
-app.get('/quote/:id', (req, res) => {
-    const quoteId = parseInt(req.params.id);
-    const quote = quotes.find(q => q.id == quoteId);
-
-    if (!quote)    res.status(404).json({ message: "Quote not found" });
-    else           res.status(200).json(quote);
-    
-});
 
 app.listen(PORT, () => {
     console.log('Server is running in localhost on port 3000');
